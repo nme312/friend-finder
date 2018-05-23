67b7814c378bb43e8561eee705710ab2ad455bde
SkyNet <nme312.noreply.github.com>
2018-05-23 12:14:05 -0500

    require route files on server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,12 +8,14 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// A default, catch-all route that leads to home.html which displays the home page.
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
-
-// A GET Route to /survey which should display the survey page.
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-});
+module.exports = function(app){
+    // A default, catch-all route that leads to home.html which displays the home page.
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "home.html"));
+    });
+    
+    // A GET Route to /survey which should display the survey page.
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "survey.html"));
+    });   
+}
